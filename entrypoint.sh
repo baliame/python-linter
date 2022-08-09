b16a42e84c6b8c0913753e64745a6b2646f070da
Akos Toth <akos.toth@cheppers.com>
2022-08-09 13:42:27 +0200

    Remove the django part

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,57 +12,30 @@ shopt -s globstar
 # django = $7
 
 echo "===== Running Python Linter - '$2' ====="
-echo "Django project? '$7'"
 
 echo "Running mypy"
 mypy "$1" --show-error-codes --show-error-context --pretty ${3}
 
-echo "Running black"
-if [ "$7" ]; then
-  if [ "$2" = "high" ]; then
-    black "$1" ${5} --exclude "migrations" --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running pylint"
-    pylint "$1" --load-plugins pylint_django ${4}
+if [ "$2" = "high" ]; then
+  black "$1" ${5} --check
+  echo "Running isort"
+  isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
+  echo "Running pylint"
+  pylint "$1" ${4}
 
-  elif [ "$2" = "medium" ]; then
-    black "$1" ${5} --exclude "migrations" --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running pylint"
-    pylint "$1" --load-plugins pylint_django --fail-under=8 ${4}
-
-  else
-    black "$1" ${5} --exclude "migrations" --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} --diff
-    echo "Running pylint"
-    pylint "$1" --load-plugins pylint_django --exit-zero ${4}
-  fi
+elif [ "$2" = "medium" ]; then
+  black "$1" ${5} --check
+  echo "Running isort"
+  isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
+  echo "Running pylint"
+  pylint "$1" --fail-under=8 ${4}
 
 else
-  if [ "$2" = "high" ]; then
-    black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running pylint"
-    pylint "$1" ${4}
-
-  elif [ "$2" = "medium" ]; then
-    black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} -c
-    echo "Running pylint"
-    pylint "$1" --fail-under=8 ${4}
-
-  else
-    black "$1" ${5} --check
-    echo "Running isort"
-    isort "$1"/**/*.py -m 3 --trailing-comma ${6} --diff
-    echo "Running pylint"
-    pylint "$1" --exit-zero ${4}
-  fi
+  black "$1" ${5} --check
+  echo "Running isort"
+  isort "$1"/**/*.py -m 3 --trailing-comma ${6} --diff
+  echo "Running pylint"
+  pylint "$1" --exit-zero ${4}
 fi
 
 echo "Done 🎉" ; echo ""
